1ebae99647aa9566de843d4bf56bf93978ca8183
oliviasylee <oliviasylee@gmail.com>
2023-04-04 01:04:47 -0400

    Fix syntax error in mutation by removing colon Updated search functionality and saved book mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -15,7 +15,7 @@ import { SAVE_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 // Delete 'saveBook'
-import { searchGoogleBooks } from '../utils/API';
+// import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -45,7 +45,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      );
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -82,8 +84,8 @@ const SearchBooks = () => {
 
     try {
       // Use the saveBook mutation function to save a book by passing in the bookToSave object as variables
-      await saveBook({
-        variables: { ...bookToSave },
+      const { data } = await saveBook({
+        variables: { newBook: { ...bookToSave } },
       });
 
       // if book successfully saves to user's account, save book id to state
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -31,7 +31,7 @@ export const SAVE_BOOK = gql`
       username
       email
       bookCount
-      savedBooks: {
+      savedBooks {
         bookId
         authors
         description
@@ -50,7 +50,7 @@ export const REMOVE_BOOK = gql`
       username
       email
       bookCount
-      savedBooks: {
+      savedBooks {
         bookId
         authors
         description
