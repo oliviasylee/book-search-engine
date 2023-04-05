621345b694b7f84e8839acaee8ef63bf419ca61f
oliviasylee <oliviasylee@gmail.com>
2023-04-05 18:23:18 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googleBooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true, // Configures Mongoose to use createIndex() instead of ensureIndex().
-  useFindAndModify: false, // Sets to use findOneAndUpdate() and findOneAndDelete() instead of findAndModify().
+  // useCreateIndex: true, // Configures Mongoose to use createIndex() instead of ensureIndex().
+  // useFindAndModify: false, // Sets to use findOneAndUpdate() and findOneAndDelete() instead of findAndModify().
 });
 
 module.exports = mongoose.connection;
